8865c2ee26504cde7185dc6299b52b0208ca7560
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-01-17 18:27:47 +0000

    resume

--- src/cv.php
+++ src/cv.php
@@ -12,7 +12,7 @@
         <table>
             <tr><td style="text-align:right;">EuroPass</td><td> | <a href="#cv_europass">View</a></td> <td> | <a href="cv/krzysztof_jusiak_cv_europass.pdf">Download</a></td></tr>
             <tr><td style="text-align:right;">UK</td><td> | <a href="#cv_uk">View</a></td><td> | <a href="cv/krzysztof_jusiak_cv_uk.pdf">Download</a></td></tr>
-            <tr><td style="text-align:right;">US</td><td> | <a href="#cv_us">View</a></td><td> | <a href="cv/krzysztof_jusiak_cv_us.pdf">Download</a></td></tr>
+            <tr><td style="text-align:right;">US/AU</td><td> | <a href="#resume">View</a></td><td> | <a href="cv/krzysztof_jusiak_resume.pdf">Download</a></td></tr>
             <tr><td><hr /></td><td><hr /></td></tr>
             <tr><td style="text-align:right;">Github</td><td> | <a target="_blank" href="http://resume.github.io/?krzysztof-jusiak">View</a></td><td> | </td></tr>
         </table>
@@ -21,10 +21,12 @@
         <hr />
 
         <iframe id="cv_europass" src="http://docs.google.com/viewer?url=http%3A%2F%2Fkrzysztof-jusiak.github.io%2Fcv%2Fkrzysztof_jusiak_cv_europass.pdf&embedded=true" width="100%" height="100%" style="border: none;"></iframe>
+        <hr />
         <iframe id="cv_uk" src="http://docs.google.com/viewer?url=http%3A%2F%2Fkrzysztof-jusiak.github.io%2Fcv%2Fkrzysztof_jusiak_cv_uk.pdf&embedded=true" width="100%" height="100%" style="border: none;"></iframe>
-        <iframe id="cv_us" src="" width="100%" height="100%" style="border: none;"></iframe>'
-
-      .'</div></div>'
+        <hr />
+        <iframe id="resume" src="http://docs.google.com/viewer?url=http%3A%2F%2Fkrzysztof-jusiak.github.io%2Fcv%2Fkrzysztof_jusiak_resume.pdf&embedded=true" width="100%" height="100%" style="border: none;"></iframe>
+        <hr />
+      '.'</div></div>'
       , '</body>'
     );
 ?>
